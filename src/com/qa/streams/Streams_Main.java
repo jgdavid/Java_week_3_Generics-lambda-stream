290d85399828eb1c3b40c4c7f1a1d032a1ddc5fd
jgdavid <jdavid@qa.com>
2023-03-24 11:00:59 +0000

    added .get to min and max number methods

--- src/com/qa/streams/Streams_Main.java
+++ src/com/qa/streams/Streams_Main.java
@@ -1,34 +1,37 @@
 package com.qa.streams;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Streams_Main {
 	public static void main(String[] args) {
+		// Filter and string match to remove James from the list
 		List<String> names = List.of("Micheal", "Dean", "James", "Chris");
-		names.stream().filter(str -> str.matches("James")).collect(Collectors.toList())
-				.forEach(name -> System.out.println(name + name));
+		names.stream().filter(str -> !str.matches("James")).collect(Collectors.toList())
+				.forEach(name -> System.out.println("Hello " + name));
 
 		List<Integer> nums = List.of(3, 4, 7, 8, 12);
 
 		// Reduce method multiplies all the numbers
 		System.out.println(nums.stream().reduce(1, (a, b) -> a * b));
 
-		// Find the max of a
-		Optional<Integer> Max = nums.stream().reduce(Math::max);
-
-		Optional<Integer> Min = nums.stream().reduce(Math::min);
+		// Find the highest number
+		Integer Max = nums.stream().reduce(Math::max).get();
+		// Find the lowest number
+		Integer Min = nums.stream().reduce(Math::min).get();
+		// Find all even numbers
 		List<Integer> even = nums.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+		// Find all odd numbers
 		List<Integer> odd = nums.stream().filter(x -> x % 2 == 1).collect(Collectors.toList());
 		System.out.println(Max);
-//		System.out.println(Min);
-//		System.out.println(odd);
-//		System.out.println(even);
+		System.out.println(Min);
+		System.out.println(odd);
+		System.out.println(even);
+		// two methods of getting the sum of all numbers
 		System.out.println(nums.stream().reduce(0, (a, b) -> a + b));
 		System.out.println(nums.stream().mapToInt(i -> i.intValue()).sum());
-
-		System.out.println(nums.stream().map(num -> num * num).filter(x -> x % 2 == 0).collect(Collectors.toList()));
+		// Squared the numbers removed the even ones and then got the smallest number
+		System.out.println(nums.stream().map(num -> num * num).filter(x -> x % 2 == 1).reduce(Math::min).get());
 //				.forEach(num -> System.out.println(num * num).filter(x -> x % 2 == 0).collect(Collectors.toList()));
 
 	}
